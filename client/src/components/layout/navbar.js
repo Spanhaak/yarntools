a3769e6cdabaa7a36559579a0b15cba0cc78ffcb
Marco Spanhaak <mspanhaak@gmail.com>
2019-07-31 22:50:42 +0200

    menu items added

--- client/src/components/layout/navbar.js
+++ client/src/components/layout/navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Logo from '../img/logowhite.png';
 import LogoTab from '../img/tab.png';
+import { Link } from 'react-router-dom';
 
 
 const Navbar = ({ title, tab}) => {
@@ -10,6 +11,17 @@ const Navbar = ({ title, tab}) => {
             <h1 className="vCenterItems">
                 <img src={Logo} width="60px" height="60px" alt="" /><img src={LogoTab} width="20px" height="20px" alt="" /> {tab} {title}
             </h1>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/tools'>Tools</Link>
+                </li>
+                <li>
+                    <Link to='/about'>About</Link>
+                </li>
+            </ul>
 
         </div>
     )
