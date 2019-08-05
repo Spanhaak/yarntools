a79601422f42fd7c35709b5f97444863bc2fdff2
Marco Spanhaak <mspanhaak@gmail.com>
2019-08-06 00:43:45 +0200

    added new file

--- client/src/components/pages/About.js
+++ client/src/components/pages/About.js
@@ -5,7 +5,13 @@ const About = () => {
         <div>
             <h1>About Yarntools</h1>
             <p className="my-1">
-                This is a full stack React APP for Yarntools
+                This APP is the result of a brain storming session between Ella van Dop and Marco Spanhaak.
+            </p>
+            <p className="my-1">
+                The goal of the APP is to support crafters with tools they need to be effective in crafting. This is the reason why we maintain list of sizes, converters etc.
+            </p>
+            <p className="my-1">
+                If you have any feedback, or have a feature request, please send the request to mspanhaak@gmail.com
             </p>
             <p className='bg-dark p'>
                 <strong>Version: 0.0.1</strong>
